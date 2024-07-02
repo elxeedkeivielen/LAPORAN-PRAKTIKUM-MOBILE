562f7ebc7e74e3c1d67c493ab479a3598901706d
elxeedkeivielen <115568456+elxeedkeivielen@users.noreply.github.com>
2024-07-02 11:35:13 +0800

    Add files via upload

---
+++ modul 5/Modul 5/README.md
@@ -0,0 +1,3 @@
+# Praktikum Modul 5 Pemrograman Mobile
+
+E-Commerce App Android UI Design
---
+++ modul 5/Modul 5/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/NavigationHost.kt
@@ -0,0 +1,6 @@
+package com.google.codelabs.mdc.kotlin.shrine
+
+import androidx.fragment.app.Fragment
+interface NavigationHost {
+    fun navigateTo(fragment: Fragment, addToBackstack: Boolean)
+}
